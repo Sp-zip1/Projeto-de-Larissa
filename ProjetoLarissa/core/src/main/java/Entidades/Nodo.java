0e2d0aedd78ef39ba660651923a4f5fa1fab8d04
Sp-zip1 <xoudaxuxa.cb@gmail.com>
2025-10-11 10:44:33 -0300

    Criação do mapa

---
+++ ProjetoLarissa/core/src/main/java/Entidades/Nodo.java
@@ -0,0 +1,37 @@
+package Entidades;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Nodo {
+    public Vector2 position;
+    public Texture texture;
+    public List<Nodo> connectedNodes;
+    public boolean unlocked = false;
+    public boolean completed = false;
+    //Nodos do mapa que servem para representar fases
+    public Nodo(float x, float y, Texture texture) {
+        this.position = new Vector2(x, y);
+        this.texture = texture;
+        this.connectedNodes = new ArrayList<>();
+    }
+
+    public void connect(Nodo other) {
+        if (!connectedNodes.contains(other)) {
+            connectedNodes.add(other);
+            other.connectedNodes.add(this); // conexão bidirecional
+        }
+    }
+
+    public void draw(SpriteBatch batch) {
+        batch.draw(texture, position.x - 32, position.y - 32, 64, 64);
+    }
+
+    public boolean isClicked(float x, float y) {
+        return x > position.x - 32 && x < position.x + 32 && y > position.y - 32 && y < position.y + 32;
+    }
+}
--- ProjetoLarissa/core/src/main/java/Telas/TelaMapa.java
+++ ProjetoLarissa/core/src/main/java/Telas/TelaMapa.java
@@ -1,40 +1,99 @@
 package Telas;
 
+import Entidades.Nodo;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TelaMapa implements Screen {
-    @Override
-    public void show() {
 
+    private SpriteBatch batch;
+    private ShapeRenderer shapeRenderer;
+    private List<Nodo> nodes;
+    private Nodo currentNode;
+
+    public TelaMapa() {
+        batch = new SpriteBatch();
+        shapeRenderer = new ShapeRenderer();
+        nodes = new ArrayList<>();
+
+        Texture nodeTexture = new Texture("slice.png");
+
+        Nodo n1 = new Nodo(200, 150, nodeTexture);
+        Nodo n2 = new Nodo(400, 300, nodeTexture);
+        Nodo n3 = new Nodo(600, 150, nodeTexture);
+
+        n1.connect(n2);
+        n2.connect(n3);
+
+        n1.unlocked = true;
+        currentNode = n1;
+
+        nodes.add(n1);
+        nodes.add(n2);
+        nodes.add(n3);
     }
 
     @Override
-    public void render(float v) {
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // Desenha conexões
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.LIGHT_GRAY);
+        for (Nodo n : nodes) {
+            for (Nodo c : n.connectedNodes) {
+                shapeRenderer.line(n.position.x, n.position.y, c.position.x, c.position.y);
+            }
+        }
+        shapeRenderer.end();
+
+        // Desenha nós
+        batch.begin();
+        for (Nodo n : nodes) {
+            if (n.unlocked)
+                batch.setColor(Color.WHITE);
+            else
+                batch.setColor(Color.DARK_GRAY);
+
+            n.draw(batch);
+        }
+        batch.end();
+
+        // Detecta clique
+        if (Gdx.input.justTouched()) {
+            float x = Gdx.input.getX();
+            float y = Gdx.graphics.getHeight() - Gdx.input.getY(); // converte Y
+            for (Nodo n : nodes) {
+                if (n.isClicked(x, y) && n.unlocked) {
+                    currentNode = n;
+                    n.completed = true;
+                    // desbloqueia nós conectados
+                    for (Nodo c : n.connectedNodes) {
+                        c.unlocked = true;
+                    }
+                }
+            }
+        }
     }
 
-    @Override
-    public void resize(int i, int i1) {
-
+    @Override public void dispose() {
+        batch.dispose();
+        shapeRenderer.dispose();
+        for (Nodo n : nodes) n.texture.dispose();
     }
-
-    @Override
-    public void pause() {
-
+    @Override public void show() {
     }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
-    }
-
-    @Override
-    public void dispose() {
-
-    }
+    @Override public void resize(int width, int height) {}
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() {}
 }
