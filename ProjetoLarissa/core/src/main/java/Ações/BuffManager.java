bb0c28554d25f3e313eec9fce2bae73d597c4cc4
Sp-zip1 <dantassilas3@gmail.com>
2025-10-22 13:17:22 -0300

    Inicio de reorganização do sistema de efeitos

--- ProjetoLarissa/core/src/main/java/Ações/BuffManager.java
+++ ProjetoLarissa/core/src/main/java/Ações/BuffManager.java
@@ -1,5 +1,6 @@
 package Ações;
 
+import Atores.Inimigo;
 import Atores.Jogador;
 import Entidades.Carta;
 
@@ -8,6 +9,7 @@ import java.util.List;
 import java.util.function.BiConsumer;
 
 public class BuffManager {
+    private final List<BuffBase> buffs = new ArrayList<>();
     public static class BuffBase{}
     public static class BuffTemporario extends BuffBase {
         private final BiConsumer<Jogador, Carta> acao;
@@ -28,9 +30,58 @@ public class BuffManager {
             return usado;
         }
     }
-private final List<BuffBase> buffs = new ArrayList<>();
+    public static class BuffDuracao extends BuffBase {
+        private final BiConsumer<Jogador, Inimigo> acaoPorTurno;
+        private int turnosRestantes;
+
+        public BuffDuracao(BiConsumer<Jogador, Inimigo> acaoPorTurno, int turnos) {
+            this.acaoPorTurno = acaoPorTurno;
+            this.turnosRestantes = turnos;
+        }
+        public void aplicarPorTurno(Jogador jogador, Inimigo inimigo) {
+            if (turnosRestantes > 0) {
+                acaoPorTurno.accept(jogador, inimigo);
+                turnosRestantes--;
+            }
+        }
+        public boolean terminou() {
+            return turnosRestantes <= 0;
+        }
+        public int getTurnosRestantes() {
+            return turnosRestantes;
+        }
+    }
 public void adicionarBuffTemp(BiConsumer<Jogador, Carta> acao){
     buffs.add(new BuffTemporario(acao));
 }
-
+public void executarBuffTemp(Jogador j, Carta c){
+    List<BuffBase> ativos = new ArrayList<>(buffs);
+    for (BuffBase buff : ativos) {
+        if (buff instanceof BuffTemporario) {
+            ((BuffTemporario) buff).aplicar(j, c);
+        }
+    }
+    buffs.removeIf(buff -> buff instanceof BuffTemporario && ((BuffTemporario) buff).jaUsou());
+}
+    public void aplicarBuffsDeTurno(Jogador jogador, Inimigo inimigo) {
+        List<BuffDuracao> buffsTurno = new ArrayList<>();
+        for (BuffBase buff : buffs) {
+            if (buff instanceof BuffDuracao) {
+                buffsTurno.add((BuffDuracao) buff);
+            }
+        }
+        for (BuffDuracao buff : buffsTurno) {
+            buff.aplicarPorTurno(jogador, inimigo);
+        }
+        buffs.removeIf(buff -> buff instanceof BuffDuracao && ((BuffDuracao) buff).terminou());
+    }
+    public void limparBuffs() {
+        buffs.clear();
+    }
+    public int contarBuffs() {
+        return buffs.size();
+    }
+    public List<BuffBase> getBuffs() {
+        return new ArrayList<>(buffs);
+    }
 }
